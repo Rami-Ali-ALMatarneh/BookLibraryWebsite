e53df5feaad3a5fd415ec7ada9656cf777801ddb
hp <hp@LAPTOP-DH29EFJI>
2023-06-19 17:04:47 +0300

    update 5:04PM 19/6/2023

--- BookLibraryWebsite/Controllers/HomeController.cs
+++ BookLibraryWebsite/Controllers/HomeController.cs
@@ -8,7 +8,11 @@ namespace BookLibraryWebsite.Controllers
         //inject IBookRepository & IWebHostEnvironment
         private readonly IBookRepository _bookRepository;
         private readonly IWebHostEnvironment _webHostEnvironment;
-
+        public HomeController( IBookRepository _bookRepository, IWebHostEnvironment _webHostEnvironment )
+        {
+            this._bookRepository = _bookRepository;
+            this._webHostEnvironment = _webHostEnvironment;
+        }
         public IActionResult Index()
             {
             return View();
