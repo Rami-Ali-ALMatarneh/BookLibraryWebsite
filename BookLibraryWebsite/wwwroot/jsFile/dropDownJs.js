8b3d9f1e02475e0535f4761943ddba93e4b97931
hp <hp@LAPTOP-DH29EFJI>
2023-06-21 04:30:46 +0300

    Design Landing page V1 6:47PM 20/6/2023

--- BookLibraryWebsite/wwwroot/jsFile/dropDownJs.js
+++ BookLibraryWebsite/wwwroot/jsFile/dropDownJs.js
@@ -1,5 +1,6 @@
 ﻿let dropDown = document.getElementById("dropListIcon");
 let dropSpan = document.querySelectorAll(".dropListIcon span");
+let navbarTo = document.querySelector(".navbar");
 let flag = false;
 dropDown.onclick = function () {
     if (flag == false) {
@@ -10,7 +11,12 @@ dropDown.onclick = function () {
         dropSpan[0].classList.remove("backUp");
         dropSpan[1].classList.remove("dropView");
         dropSpan[2].classList.remove("backDown");
+        /////////////////////////////////////
+        navbarTo.classList.add("navbarToRight");
+        navbarTo.classList.remove("navbarToLeft");
         flag = true;
+
+
     }
     else {
         dropSpan[0].classList.remove("goDown");
@@ -20,6 +26,9 @@ dropDown.onclick = function () {
         dropSpan[0].classList.add("backUp");
         dropSpan[1].classList.add("dropView");
         dropSpan[2].classList.add("backDown");
+        /////////////////////////////////////
+        navbarTo.classList.remove("navbarToRight");
+        navbarTo.classList.add("navbarToLeft");
         flag = false;
     }
 }
