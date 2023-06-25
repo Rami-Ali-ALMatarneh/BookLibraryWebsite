c56ee2187b6ddfec3aff7632c0e6eed9203004ec
hp <hp@LAPTOP-DH29EFJI>
2023-06-25 03:57:19 +0300

    Development & Design Version 3:58AM  25/6/2023

--- BookLibraryWebsite/wwwroot/jsFile/AnimationJS.js
+++ BookLibraryWebsite/wwwroot/jsFile/AnimationJS.js
@@ -1,4 +1,9 @@
 ﻿let ServiceBox = document.querySelectorAll(".ServiceBox ");
+let toUp = document.querySelector(".toUp");
+//////////////////////////////////////////
+let CountNumber = document.getElementById("CountNumbers");
+let countN = document.querySelectorAll(".numbers");
+let flagCount = false;
 window.onload = setAnimation();
 function setAnimation() {
     window.onscroll = function () {
@@ -7,5 +12,39 @@ function setAnimation() {
                 e.classList.add("goDownAnimation");
             })
         }
+        if (window.scrollY > 200) {
+            toUp.classList.add("toLeft");
+            toUp.classList.remove("toRight");
+
+        }
+        else
+        {
+            toUp.classList.add("toRight");
+            toUp.classList.remove("toLeft");
+
+        }
+        if (window.scrollY >= CountNumber.offsetTop -300) {
+            if (!flagCount) {
+                countN.forEach((e) => {
+                    countingNumber(e);
+                    flagCount = true;
+                });
+            }
+        }
     }
+}
+function countingNumber(e) {
+    let goal = e.dataset.numbers;
+    let InitialNum = 0;
+    let count = setInterval(() => {
+        if (InitialNum != goal) {
+            e.innerHTML = InitialNum++;
+        }
+        else {
+            clearInterval(count);
+        }
+    }, 2*0.10/goal);
+}
+toUp.onclick = function () {
+    window.scrollTo(0,0);
 }
